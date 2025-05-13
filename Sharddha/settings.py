10434c231d2ae5aed207247d4672a341ca7bc5b7
YuG281205 <yugjhaveri1234@gmail.com>
2025-05-13 14:39:51 +0530

    Fix: unique static filenames and collected

--- Sharddha/settings.py
+++ Sharddha/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l=sa^cgt$zd4qp2(xp(+3dh!s80n8-(3e@y16i*5wr$l#e6meo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shraddha-7.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['shraddha-8.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
